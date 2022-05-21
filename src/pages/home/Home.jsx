a9803e4d0a2f60556fdb7f50204921647938a9f4
Mathmci13 <mathmci002@gmail.com>
2022-05-20 21:33:18 -0300

    Inclusão dos primeiros gráficos e melhoras em geral

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/Widget/Widget'
 import Featured from '../../components/featured/Featured'
 import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 import "./home.scss"
 
 const Home = () => {
@@ -21,6 +22,10 @@ const Home = () => {
           <div className="charts">
             <Featured/>
             <Chart/>
+          </div>
+          <div className="listContainer">
+            <div className="listTitle">Últimas Transações</div>
+            <Table/>
           </div>
         </div>
     </div>
